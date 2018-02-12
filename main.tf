f028450a904e44244f2731276941500db0090220
pintbrewer <brewdroid+github@gmail.com>
2018-02-12 07:53:50 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
-resource "aws_s3_bucket" "new_bucket" {
+uresource "aws_s3_bucket" "new_bucket" {
 bucket = "draco-tf-test-bucket"
 acl    = "private"
-
+region = "us-east-1"
+    
 tags {
     Name        = "My bucket"
     Environment = "Dev"
